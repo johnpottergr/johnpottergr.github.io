172b8da42df85117efacb10d9196dc1d821b4b5e
John Potter <john.m.potter@protonmail.com>
2025-07-05 18:56:21 -0400

    150

--- src/pages/career.js
+++ src/pages/career.js
@@ -85,7 +85,7 @@ export default function Workflow() {
     style={{
       height: '70px',
       marginTop: '10px',
-      marginRight: '250px',
+      marginRight: '150px',
       flex: '0 0 auto',
     }}
   />
