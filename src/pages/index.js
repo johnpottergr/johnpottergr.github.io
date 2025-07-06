9a4292a0d3fc6c3abefddd8a5e41a5f952df846d
John Potter <john.m.potter@protonmail.com>
2025-07-06 00:29:45 -0400

    green line

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,14 @@ export default function Home() {
 
          {/* New Section */}
          <div>
-
+  <p
+  style={{
+    borderBottom: '2px solid #5ED1A5',
+    paddingBottom: '16px',
+    marginBottom: '1.5rem',
+  }}
+>
+</p>
 
 <iframe width="560" height="315" src="https://www.youtube.com/embed/7DYhiD3IuxE?si=F3y8zcWtsqVwiwPs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
