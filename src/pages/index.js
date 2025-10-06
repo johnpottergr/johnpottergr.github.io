00cfaae2a863322e25e7d7ce2ccc5269904cd7e2
John Potter <john.m.potter@protonmail.com>
2025-10-05 20:12:26 -0400

    added

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ Omniscient Digital</a> to improve their clients’
 
     </p>
 
-    <p>My newsletter can be found <a href="https://www.linkedin.com/newsletters/founders-onchain-capital-6976225311034138624/">on LinkedIn</a></p>
+    <p>My newsletter can be found on <a href="https://www.linkedin.com/newsletters/founders-onchain-capital-6976225311034138624/">on LinkedIn</a></p>
 
 
   </div>
@@ -79,7 +79,7 @@ Omniscient Digital</a> to improve their clients’
   </div>
 </div>
 
-         {/* New Section */}
+  {/* New Section */}
   <div>
     <div
   style={{
